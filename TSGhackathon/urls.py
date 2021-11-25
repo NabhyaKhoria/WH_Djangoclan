4f17151669074886567ca09184447d1ee86c723b
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2021-11-25 12:45:05 +0530

    base.html created

--- TSGhackathon/urls.py
+++ TSGhackathon/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('admin/', admin.site.urls),
+    path('base/',views.base)
 ]
--- TSGhackathon/views.py
+++ TSGhackathon/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render,redirect
+
 def home(request):
     return render(request, 'home.html')
+def base(request):
+    return render(request,'base.html')
