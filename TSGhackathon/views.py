49985bed83cf6c20f15623e1365d86949397fbc2
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2021-12-07 01:06:11 +0530

    Validation Error in Mail

--- TSGhackathon/views.py
+++ TSGhackathon/views.py
@@ -29,35 +29,35 @@ def base(request):
 
 def login(request):
     if(request.method == 'POST'):
-        email = request.POST['signin-email']
-        string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
         OTP = ""
-        length = len(string)
-        for i in range(6) :
-            OTP += string[math.floor(random.random() * length)]
-        subject = 'One Time Password (OTP) to login at TSG'
-        email2 = EmailMessage(
-						subject, OTP, settings.EMAIL_HOST_USER, to=[email]
-					)
-        email2.send(fail_silently=True)
-        return render(request,'baselogin.html', {'email':email,'OTP':OTP})
+        email = request.POST['signin-email']
+        wb_obj = openpyxl.load_workbook("media\student\Student Data.xlsx")
+        sheet_obj = wb_obj.active
+        semail=[]
+        for j in range(3,sheet_obj.max_row+1):
+            cell_obj = sheet_obj.cell(row = j, column = 1)
+            semail.append(cell_obj.value)
+        val=0
+        print(semail)
+        if semail.count(email):
+            val=1
+            string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            length = len(string)
+            for i in range(6) :
+                OTP += string[math.floor(random.random() * length)]
+            subject = 'One Time Password (OTP) to login at TSG'
+            email2 = EmailMessage(
+                            subject, OTP, settings.EMAIL_HOST_USER, to=[email]
+                        )
+            email2.send(fail_silently=True)
+        return render(request,'baselogin.html', {'email':email,'OTP':OTP,'val':val})
     else:
         return render(request,'base.html')
 
 def loginuser(request):
     if(request.method == 'POST'):
         email = request.POST['signin-email']
-        wb_obj = openpyxl.load_workbook("media\student\Student Data.xlsx")
-        sheet_obj = wb_obj.active
-        semail=[]
-        for j in range(sheet_obj.max_row+1,3):
-            cell_obj = sheet_obj.cell(row = j, column = 1)
-            semail.append(cell_obj.value)
-        val=0
-        if semail.count(email):
-            val=1
-        
-        OTP = ""
-        return render(request,'baselogin.html', {'email':email,'OTP':OTP,'val':val})
+        OTP=""
+        return render(request,'baselogin.html', {'email':email,'OTP':OTP})
     else:
         return render(request,'base.html')
