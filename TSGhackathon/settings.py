4ba57ee5a6bfacdaa40c7449ce3506d4fca18edd
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2022-01-04 14:38:30 +0530

    Css Issue solved

--- TSGhackathon/settings.py
+++ TSGhackathon/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5pavp3_kdy&9i9bf9qch6f*60ew$$+^g)f*%3y5=l0+c=-z!0#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
@@ -141,4 +140,3 @@ EMAIL_HOST_PASSWORD = 'Qwerty@123'
 AUTH_USER_MODEL = 'users.User'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
